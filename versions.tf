af84fe5c5d851f3d00de9441349088eddccaacab
hkalii <hamza.znd1@gmail.com>
2021-10-16 20:26:45 +0100

    test

--- versions.tf
+++ versions.tf
@@ -3,9 +3,9 @@ terraform {
     google = {
       source  = "hashicorp/google"
       version = "3.52.0"
-    }
+      credentials = "/home/workstation/.config/gcloud/application_default_credentials.json"
   }
-
+  }
   required_version = "~> 0.14"
 }
 
